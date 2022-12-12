25cdd570a699e04b46f9a49aa82f36baa2393441
Romero <romeromedeiros5@gmail.com>
2022-12-12 17:27:03 -0300

    Intentionally breaks the test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const { mooncakeSays } = require('./');
 
 test('mooncake knows what to say', () => {
-  expect(mooncakeSays('woooahhh!')).toBe('(o.o) woooahhh!');
+  expect(mooncakeSays('woooahhh!')).toBe('(o.o) woooahhh!!');
 });
 
 test('mooncake is not sure what to say', () => {
